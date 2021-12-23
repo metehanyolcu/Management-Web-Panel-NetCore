681ff800a186380165f4ac2fe23158ec4f1507e8
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-23 16:11:51 +0300

    SET DA(Category)

--- Project/ManagementSystem/MY.MS.Entities/Concrete/Categories.cs
+++ Project/ManagementSystem/MY.MS.Entities/Concrete/Categories.cs
@@ -1,13 +1,21 @@
 ﻿using MY.MS.Entities.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MY.MS.Entities.Concrete
 {
+    [Table("Categories")]
     public class Categories : ITable
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CategoryID { get; set; }
+        [Column("CatName")]
+        [Required]
+        [StringLength(40)]
         public string CategoryName { get; set; }
        
         public List<Items> Items { get; set; }
