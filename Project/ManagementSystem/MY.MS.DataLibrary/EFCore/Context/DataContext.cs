e6b9c3b7b72b9905d5cb05f3674de1b00c405107
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-23 16:44:43 +0300

    Create Context
    Creating context class

---
+++ Project/ManagementSystem/MY.MS.DataLibrary/EFCore/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MY.MS.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MY.MS.DataLibrary.EFCore.Context
+{
+    internal class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server  = METEPC06; database = MSDemo; user id = sa; password = 1234;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>().HasIndex(c => new { c.CategoryName }).IsUnique();
+
+        }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Items> Items { get; set; }
+    }
+}
