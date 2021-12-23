b090a351a55822541a960cc926ed8df144ef1478
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-23 15:58:31 +0300

    Data Annotations
    Set data annotations in user class

--- Project/ManagementSystem/MY.MS.Entities/Concrete/Users.cs
+++ Project/ManagementSystem/MY.MS.Entities/Concrete/Users.cs
@@ -1,18 +1,50 @@
 ﻿using MY.MS.Entities.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MY.MS.Entities.Concrete
 {
+    //Set Table Name
+    [Table("UsersT")]
     public class Users : ITable
     {
+        //Set Primary Key
+        [Key]
+        //Set identity attributes
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; }
+        //Required = Not Null
+        [Required]
+        //Set Column Name
+        [Column("Username")]
+        //Set nvarchar and max length
+        [StringLength(50)]
+        //Set minimum length
+        [MinLength(2)]
         public string UserName { get; set; }
+        [Required]
+        [Column("Password")]
+        [StringLength(80)]
+        [MinLength(6)]
         public string Password { get; set; }
+        [Required]
+        [Column("Email")]
+        [StringLength(150)]
         public string EMail { get; set; }
+        [Required]
+        [Column("Name")]
+        [StringLength(50)]
+        [MinLength(2)]
         public string Name { get; set; }
+        [Required]
+        [Column("Surname")]
+        [StringLength(50)]
+        [MinLength(2)]
         public string SurName { get; set; }
-        public string Age { get; set; }
+        [Required]
+        public int Age { get; set; }
     }
 }
