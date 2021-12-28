078d03aac37e933d54aa4c786dea2be1eda86fb6
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-28 14:46:41 +0300

    BL Create users class

---
+++ Project/ManagementSystem/MY.MS.BusinessLayer/Repisotory/Concrete/C_Users.cs
@@ -0,0 +1,59 @@
+﻿using MY.MS.BusinessLayer.Repisotory.Abstract;
+using MY.MS.DataLibrary.RepisotoryDP.Concrete;
+using MY.MS.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MY.MS.BusinessLayer.Repisotory.Concrete
+{
+    public class C_Users : IBusinessC<Users>
+    {
+        private readonly ConcreteUsers usersdb;
+        public C_Users()
+        {
+            usersdb = new ConcreteUsers();
+        }
+        public void Add(Users t)
+        {
+            usersdb.Add(t);
+        }
+
+        public Users GetT(int id)
+        {
+            return  usersdb.GetT(id);
+        }
+
+        public List<Users> ListAll()
+        {
+            return usersdb.ListAll();
+        }
+
+        public List<Users> ListByID(int id)
+        {
+            return usersdb.ListByID(id);
+        }
+
+        public void ListByName(string name)
+        {
+           usersdb.ListByName(name);
+        }
+
+        public void Remove(Users t)
+        {
+            usersdb.Remove(t);
+        }
+
+        public void Update(Users t)
+        {
+            usersdb.Update(t);
+        }
+
+        public void UpdateByID(int id)
+        {
+            usersdb.UpdateByID(id);
+        }
+    }
+}
