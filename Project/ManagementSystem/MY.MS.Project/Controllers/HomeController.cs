56f8106a39bd056d1c6b9145061308da8821df80
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-29 16:44:01 +0300

    Adding view

---
+++ Project/ManagementSystem/MY.MS.Project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MY.MS.Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
