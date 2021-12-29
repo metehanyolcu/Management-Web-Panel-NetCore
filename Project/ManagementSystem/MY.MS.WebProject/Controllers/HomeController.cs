32c8b9f9d652e538dbdb42fa48c3d669ac231213
Metehan Yolcu <metehanyolcu0q@outlook.com>
2021-12-29 23:26:17 +0300

    U5

---
+++ Project/ManagementSystem/MY.MS.WebProject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MY.MS.WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
